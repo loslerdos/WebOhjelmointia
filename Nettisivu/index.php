75f6f76c37a5095f5f53fdf772ebf5a9755ced71
loslerdos <sami.lehtinen@vuoksi.fi>
2026-09-11 12:59:02 +0300

    tehtava1 valmis

--- Nettisivu/index.php
+++ Nettisivu/index.php
@@ -1,13 +1,18 @@
-<?php
-
-$name = "Matti";
-
-$year = 2026;
-
-?>
-
-
-
-<h1>Hello, <?= $name ?>!</h1>
-
-<p>The year is <?= $year ?>.</p>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ensimmäinen PHP-sivu</title>
+  </head>
+  <body>
+    <p>Vuorokaudessa on <?php echo 24 * 60 * 60; ?> sekuntia.</p>
+    <p>Tänään on <?php echo date("j.n.Y"); ?>.</p>
+    <p>Palvelimella on PHP:n versio <?php echo PHP_VERSION; ?>.</p>
+    <?php
+    echo "<ul>";
+    for ($i = 1; $i <= 10; $i++) {
+        echo "<li>" . $i;
+    }
+    echo "</ul>";
+    ?>
+  </body>
+</html>
